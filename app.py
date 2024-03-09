8227e9561b45633bc4b9cc2bf101fa5508487584
diallo75012 <11600022@students.koi.edu.au>
2024-03-09 17:03:19 +0100

    starting to integrate pgvector embeddings and langchain chunking int eh process with streamlit frontend blocks to handle the user interations with preview of the document uploaded behind a toggle button. added some dummy documents for testing and create manually the postgresql database and activated the extension for langchain to be able to store embeddings. collection name have been set in the code to be the document name. need to think in the future in what is best way to chunk because it is the most important part of RAG (preparation of document  with plan to embed it nowing in advance which technique is gonna be used in order to prepare the doc the best. The retrieval process used also have to be known in advance in order to prepare the document to be embedded. My idea here is that document embedded need to be strategically prepared know both chunking technique used and retrieval algorithm used.if chunking is not done properly the retrieval will be poor/)

--- app.py
+++ app.py
@@ -1,19 +1,186 @@
+#for env access and app
 import streamlit as st
+import os
+from dotenv import load_dotenv
+# langchain
 from langchain_community.llms import OpenAI
+from langchain_groq import ChatGroq # need to be installed not in community library as of 03/2024
+from langchain_openai import ChatOpenAI # if needed to mimmic Openai endpoint
+# from langchain.chat_models import HumanMessage
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader # will load text from document so no need python `with open , doc.read()`
+from langchain_community.vectorstores.pgvector import PGVector
+from langchain.vectorstores.pgvector import DistanceStrategy
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.chat_models import ChatOllama
+from langchain.chains import RetrievalQA
+from IPython.display import Markdown, display
+# langchain custom library
+import pgvector_langchain
+from pgvector_langchain import (
+  # chunk_doc, # we don't our library function but a cutomized one that is in the helper functions section 
+  vector_db_create,
+  vector_db_retrieve,
+  add_document_and_retrieve,
+  vector_db_override,
+  # create_embedding_collection, # will create a custom one here
+  similarity_search,
+  MMR_search,
+  ollama_embedding,
+  answer_retriever
+)
+# pdf and txt file handling
+from langchain_community.document_loaders import PyPDFLoader
+from io import StringIO
+import json
+
+
+load_dotenv()
+
+# env vars
+OPENAI_API_BASE=os.getenv("OPENAI_API_BASE")
+OPENAI_MODEL_NAME=os.getenv("OPENAI_MODEL_NAME")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+connection_string = pgvector_langchain.CONNECTION_STRING
+# collection_name --> we will use file name in function to define it
+
+embeddings = pgvector_langchain.embeddings
 
 st.title('🦜🔗 Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
 
+### HELPER FUNCTIONS
+
+def generate_response(input_text):
+    print("######################  TYPE OF 'input_text' from function: ", type(input_text))
+    # llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    # llm = ChatOpenAI(temperature=0.1, model=OPENAI_MODEL_NAME, max_tokens=1024)
+    llm = ChatGroq(temperature=0.1, groq_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL_NAME) # mixtral-8x7b groq llm
+    st.info(llm.predict(input_text)) # use llm.predict(input_text) as llm(input_text) like in the documentation won't work
+
+def store_embeddings(input_text, embedding_llm_engine):
+    print("######################  TYPE OF 'input_text' from function: ", type(input_text))
+    llm = embedding_llm_engine
+    st.info() # use llm.predict(input_text) as llm(input_text) like in the documentation won't work
+
+def chunk_doc_text(text_of_doc: str) -> list:
+  if text_of_doc != "" and text_of_doc is not None:
+    try:
+      # using CharaterTextSplitter (use separator split text)
+      # text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=200, chunk_overlap=20)
+      # using RecursiveCharacterTextSplitter (maybe better)
+      text_splitter = RecursiveCharacterTextSplitter(chunk_size=230, chunk_overlap=20) # instance create to chunk doc or text
+      list_docs = [Document(page_content=x) for x in text_splitter.split_text(text_of_doc)] # instead of method .split_documents here we use .split_text to chunk instance
+      print("List Docs: ", list_docs)
+    except Exception as e:
+      print("Chunk Doc Error: ", e)
+      return e
+  print(f"Doc: {list_docs}\nLenght list_docs: {len(list_docs)}")
+  return list_docs
+
+#all_docs = chunk_doc(text_of_doc) # list_documents_txt
+def create_embedding_collection(all_docs: list, file_name: str) -> str:
+  global connection_string
+  collection_name = file_name
+  connection_string = connection_string
+  count = 0
+  for doc in all_docs:
+    print(f"Doc number: {count}")
+    vector_db_create(doc, collection_name, connection_string) # this to create/ maybe override also
+    # vector_db_override(doc, embeddings, collection_name, connection_string) # to override
+    count += 1
+  return st.info(f"Collection created with documents: {count}")
+
+### DOC UPLOAD PART
+
+from PyPDF2 import PdfReader
+@st.cache_data
+def load_docs(file_uploaded, file_name):
+    all_text = ""
+    print("file name end: ", file_name.split(".")[1])
+    if file_name.split(".")[1] == "pdf":
+      pdf_reader = PdfReader(file_uploaded)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        all_text += text
+    elif file_name.split(".")[1] == "txt":
+      stringio = StringIO(file_uploaded.getvalue().decode("utf-8"))
+      text = stringio.read()
+      all_text += text
+    else:
+      st.warning('Please provide txt or pdf.', icon="⚠️")
+    print("All Text: ", all_text)
+    return all_text
+
+# User uploads document and embedded before querying
+doc_uploaded = st.file_uploader("Upload your file", type=[".pdf",".txt"], accept_multiple_files=False,)
+
+if doc_uploaded is not None:
+  file_name = doc_uploaded.name
+  with st.spinner("Wait while file is being uploaded..."):
+    st.info(f"File uploaded: {file_name}")
+    doc = load_docs(doc_uploaded, file_name)
+
+    on = st.toggle('Check Document')
+    if on:
+      #st.write(doc)
+      st.subheader(doc[0:400])
+    
+    st.success("Now Your Can Query To Get Insights About The Document")
+    
+    if doc:
+      with st.form("Embbed_doc"):
+        tutorial = st.info("Steps: First store the document and wait while file is being processed, then start querying document to get insights.")
+        doc_submitted = st.form_submit_button("Store Document")
+
+        if doc_submitted:
+          all_docs = chunk_doc_text(doc) # returns list of doc chuncks
+          print("ALL DOCS: ", [str(doc)+"\n" for doc in all_docs])
+          create_embedding_collection(all_docs, file_name) # make sure embedding server is running if local otherwise provide the engine env vars
+    
+  # User uploaded document head is shown to have an overview of it (call also use pd dataframe to have more control on how many row to show if csv file for eg.)
+  #with st.spinner("Wait while file is being uploaded..."):
+    #loader = PyPDFLoader(json.load(doc_uploaded).upload_url)
+    #pages = loader.load_and_split()
+    #st.write(pages[0])
+    # bytes_data = doc_uploaded.getvalue()
+    # .subheader or use .write
+    # head_doc_uploaded = st.subheader(bytes_data.decode("utf-8")) # .decode("utf-8") for .txt files
+    #st.success("Now Your Can Query To Get Insights About The Document")
+    #st.warning("Make sure the uploaded document type is supported (ppdf, txt)", icon="🔥")
+
+#### FORM WITH USER PROMPTING LOGIC TO LLM
+# need to add logic to query embeddings and maybe use agent crew to work
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
+    ### USER PROMPT PART
+    # User entering text
+    text = st.text_area("Enter text:", "What are the main findings in this paper?")
+    submitted = st.form_submit_button("Submit")
+
+    # message warning if openai_api_key hasn't been provider by User
+    if not openai_api_key.startswith("gsk_"): # 'gsk-' for groq and 'sk-' for openai
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+        junko = "SIBUYAAAAA"
+        st.info(junko, icon="🔥") # success, warning, info, exception(only in try except and no 'icon')
+        #st.balloons()
+        #st.snow()
+
+    #### LLM ANSWER PART
+    # LLM answer to User prompt
+    if submitted and openai_api_key.startswith("gsk_"):
+        #message = HumanMessage(content=f"{text}")
+        #print("######################  TYPE OF 'message': ", type(message))
         generate_response(text)
+
+
+
+
+
+
+
 
--- pgvector_langchain.py
+++ pgvector_langchain.py
@@ -1,30 +1,18 @@
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader # will load text from document so no need python `with open , doc.read()`
-from langchain_community.vectorstores.pgvector import PGVector, _get_embedding_collection_store
-# from langchain_community.vectorstores import Lantern # OR using Lantern extension for cosine similarity search
+from langchain_community.vectorstores.pgvector import PGVector
 from langchain.vectorstores.pgvector import DistanceStrategy
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings # if needed to mimmic openai
 from langchain_community.embeddings import OllamaEmbeddings
 import os
 from dotenv import load_dotenv
-from flask import Flask, request
-from database import init_db, SessionLocal, session, engine
-from models import PgRecord, ConversationEmbedding
-from flask import Flask, request, jsonify
 from langchain_community.chat_models import ChatOllama
 from langchain.chains import RetrievalQA
 from IPython.display import Markdown, display
-from sqlalchemy.orm import Session
 
-### SET UP ENV VARS, FLASK APP, DATABASE INIT, OPENAIKEY
-
-# load env vars
+### SET UP ENV VARS, OPENAIKEY
 load_dotenv()
-# create flask app
-app = Flask(__name__)
-# Initialize the database
-init_db()
 # Openai key
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
@@ -34,7 +22,7 @@ os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 list_documents_txt = ["cardano_meme_coin.txt", "best_meme_coins_2024.txt", "history_of_coins.txt", "article.txt"]
 
 # Use ollama to create embeddings
-embeddings = OllamaEmbeddings(temperature=0) # for Lantern embeddings need to do some manipulation and set "options": {"m_embd": 1536}, doesn't work here, need to change it in the ollama native package manually at line 188 and 469 approx. /home/creditizens/voice_llm/voice_venv/lib/python3.10/site-packages/ollama/_client.py
+embeddings = OllamaEmbeddings(temperature=0)
 
 # define connection to pgvector database
 CONNECTION_STRING = PGVector.connection_string_from_db_params(
@@ -55,7 +43,7 @@ def chunk_doc(path: str, files: list) -> list:
   for file in files:
     loader = TextLoader(f"{path}/{file}")
     documents = loader.load()
-    # using CharaterTextSplitter
+    # using CharaterTextSplitter (use separator split text)
     # text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=200, chunk_overlap=20)
     # using RecursiveCharacterTextSplitter (maybe better)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=230, chunk_overlap=20)
@@ -74,19 +62,6 @@ def vector_db_create(doc, collection, connection):
     distance_strategy = DistanceStrategy.COSINE,
   )
   return db_create
-
-# OR using lantern (default cosine similarity search) Need also to go to change the ADA_TOKEN_COUNT to 4096 for the embedding dimension (defaut is 1536)
-# change it here line 40 change the value of it: /home/creditizens/voice_llm/voice_venv/lib/python3.10/site-packages/langchain_community/vectorstores/lantern.py 
-def lantern_db_create_or_override(doc, collection, connection):
-  lantern_db_create_or_override = Lantern.from_documents(
-    embedding=embeddings,
-    documents=doc,
-    collection_name=collection,
-    connection_string=connection,
-    distance_strategy="cosine", # can be "eucledian", "hamming", "cosine"EUCLEDIAN, COSINE, HAMMING
-    pre_delete_collection=True, # will delete collection if exist so create it again with new embeddings
-  )
-  return lantern_db_create_or_override
 
 # PGVector retriever
 def vector_db_retrieve(collection, connection, embedding):
@@ -127,7 +102,6 @@ def create_embedding_collection(all_docs: list) -> str:
     print(f"Doc number: {count} with lenght: {len(doc)}")
     vector_db_create(doc, collection_name, connection_string) # this to create/ maybe override also
     # vector_db_override(doc, embeddings, collection_name, connection_string) # to override
-    # lantern_db_create_or_override(doc, collection_name, connection_string) # to use lantern instead of pgvector and have cosine similarity search by default
     count += 1
   return f"Collection created with documents: {count}"
 # print(create_embedding_collection(all_docs))
@@ -184,69 +158,4 @@ def answer_retriever(query, collection, connection, embedding):
     "Response": response["result"],
   }
 
-# text_query = "What is coliving in Chikara Houses?"
-# print(answer_retriever(text_query, COLLECTION_NAME, CONNECTION_STRING, embeddings))
-#db = vector_db_retrieve(COLLECTION_NAME, CONNECTION_STRING, embeddings)
-#print("***********DB************: -->>\n", db.page_content)
 
-# here we get the name of collection it return collection name or None, this is to check if collection exist and know if we create or just search directly or override/update/addocument to it
-name_of_collection_to_check = "Junkette"
-def check_document_exist_or_not(name_of_collection_to_check):
-  with Session(engine) as session: # need sessions as PGVector class method takes in the sessions as parameter with the name of the collection to check
-    get_name = _get_embedding_collection_store.get_by_name()
-    collection_check = get_name(session, name_of_collection_to_check) 
-    if collection_check is None:
-      return False
-    else:
-      return True
-
-print("CHECK COLLECTION EXISTANCE True/False: ", check_document_exist_or_not(name_of_collection_to_check))
-        
-
-"""
-## OR just the route for Ollama native without langchain embeddings
-curl http://localhost:11434/api/embeddings -d '{
-  "model": "mistral:7b",
-  "prompt": "Here is an article about llamas..."
-}'
-Outputs:
-{
-  "embedding": [
-    0.5670403838157654, 0.009260174818336964, 0.23178744316101074, -0.2916173040866852, -0.8924556970596313,
-    0.8785552978515625, -0.34576427936553955, 0.5742510557174683, -0.04222835972905159, -0.137906014919281
-  ]
-}
-"""
-
-# add document to store and retrieving
-#store_db = vector_db_retrieve(COLLECTION_NAME, CONNECTION_STRING, embeddings)
-#print(store_db)
-#doc1 = add_document_and_retrieve("foo", store_db)[0] # add_document_and_retrieve("foo")[1]
-
-# if you want to override vector store need .from_documents and option pre_delete_collection=True
-#files = ["article.txt"]
-#docs = chunk_doc("/home/creditizens/voice_llm", files)
-#override_embeddings = vector_db_override(docs, embeddings, COLLECTION_NAME, CONNECTION_STRING)
-
-"""
-#### LFASK APP ROUTES "see how to pass question and answers data through a post request from another function"
-@app.route('/embedding_record_conversation/<question>/<answer>', methods=['POST'])
-def embedding_record_conversation(question, answer):
-    # Create a new session instance
-    db = SessionLocal()
-    try:
-        data = request.json
-        new_preference = UserPreference(**data)
-        db.add(new_preference)
-        db.commit()
-        return jsonify({"message": "User preference added successfully"}), 201
-    except Exception as e:
-        db.rollback()
-        return jsonify({"error": str(e)}), 400
-    finally:
-        db.close()
-
-## FLASK APP  START
-if __name__ == "__main__":
-    app.run(debug=True)
-"""
